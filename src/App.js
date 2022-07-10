2241a59f7e1e12c0a8592a0d36068dd17965f96d
ujjwalraj.exe <ujjwalrajexe@gmail.com>
2022-07-10 23:24:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Weathercard from './components/weather/weathercard';
+import Temp from './components/weather/temp';
+
+
+
+
+const App=()=> {
+  
+  return (
+    <>
+    <Temp/>
+    </>
+  )
+}
+
+export default App;
